47c271c42893b37a95705c55ad7142972a95aa33
Chandra <adityanugraha7251@gmail.com>
2025-05-27 16:38:42 +0800

    new

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- dibawah ini dua file buat css -->
-    <link rel="stylesheet" href="style/login-header.css">
-    <link rel="stylesheet" href="style/body-login.css">
+    <link rel="stylesheet" href="style/login-header.css?v=1.2">
+    <link rel="stylesheet" href="style/body-login.css?v=1.2">
 
     
     
