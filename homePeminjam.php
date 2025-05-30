848764a68cb55970aa1db9d3a9d6302e19861691
Chandra <adityanugraha7251@gmail.com>
2025-05-30 19:17:42 +0800

    halaman peminjam update

---
+++ homePeminjam.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="style/peminjam.css?v=1.2"/>
+  <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+  <title>Home</title>
+</head>
+
+<body>
+    <div class="background">
+
+        <div class="inner-background">
+
+        
+        
+            <div class="inner-header">
+
+                <div class="weather">
+                    <div class="judul">
+                        <h1>HOME</h1>
+                    </div>
+                    <div class="cuaca">
+                        <div class="forecast">
+                            <ion-icon name="partly-sunny-outline"></ion-icon>
+                        </div>
+                    <div class="suhu">
+                        <h1>23°C</h1>
+                    </div>
+                </div>
+                <p>The weather is perfect for a ride</p>
+                </div>
+
+                <div class="navigation">
+                    <div class="username">Welcome, user123456</div>
+                    <a href="profilepage.html">
+                      <div class="profile-pic"></div>
+                    </a>
+                   
+                </div>
+            </div>
+
+        <div class="inner-body">
+
+            <div class="best-deals">
+                <h2>Best Deals</h2>
+               
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+
+                        <div class="deal-texts">
+                          <h3>Motor 1</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Motor 2</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Motor 3</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+    
+ 
+
+            </div>
+
+
+            <div class="add-catalog">
+                <div class="left-column">
+                  <!-- Large element on the left -->
+                   <div class="add-catalog">
+
+                   </div>
+                   
+          
+                </div>
+              
+               
+              </div>
+
+            <div class="store-rec">
+                <div class="rec-header">
+                  <h3>Reviews</h3>
+                </div>
+                <div class="rec-body"></div>
+            </div>
+        </div>
+        </div>
+
+        <div class="sidebar">
+
+        </div>
+  </div>
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
