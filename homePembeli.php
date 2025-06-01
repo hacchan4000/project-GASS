328fd27bb56acb0146c14357d47a9c7a36c08e48
Chandra <adityanugraha7251@gmail.com>
2025-06-01 22:13:46 +0800

    update sidebar

--- homePembeli.php
+++ homePembeli.php
@@ -4,7 +4,10 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="stylesheet" href="style/homepage.css?v=1.2"/>
+  <link rel="stylesheet" href="style/sidebar.css?v=1.2">
   <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+  <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+
   <title>Home</title>
 </head>
 
@@ -162,15 +165,90 @@
           <header>
             <div class="image-text">
               <span class="image">
-                <img src="" alt="">
+                <img src="" alt="logo"><!-- masukin logo apk kita tar-->
               </span>
+
+              <div class="text header-text">
+                <span class="name">GASS</span>
+                <span class="profession">Go And Start the Street</span>
+              </div>
             </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
           </header>
+          
+          <div class="menu-bar">
+            <div class="menu">
+
+              <li class="search-box">
+                  
+                <i class='bx bx-search-alt-2 icon' ></i>
+                <input type="search" placeholder="Search...">
+                  
+              </li>
+              <ul class="menu-links">
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-home-alt icon' ></i>
+                    <span class="text nav-text">Dashboard</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="profilepage.html">
+                    <i class='bx bx-user icon' ></i>
+                    <span class="text nav-text">Profile</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-bell icon' ></i>
+                    <span class="text nav-text">Notif</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="roomchat.html">
+                    <i class='bx bx-conversation icon' ></i>
+                    <span class="text nav-text">Chat</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-store-alt icon' ></i>
+                    <span class="text nav-text">Stores</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-history icon' ></i>
+                    <span class="text nav-text">History</span>
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+
+            <div class="bottom-konten">
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-cog icon' ></i>
+                    <span class="text nav-text">Settings</span>
+                  </a>
+              </li>
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-log-out icon' ></i>
+                    <span class="text nav-text">Log out</span>
+                  </a>
+              </li>
+            </div>
+
+          </div>
         </nav>
   </div>
 
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="scripts/sidebar.js"></script>
 
 </body>
 </html>
